86c9108eff1b820dffd97becf28f97205c0ef07a
krouu <jueunheo0317@gmail.com>
2025-03-13 21:12:55 +0900

    footer 작업 완료

---
+++ widget/footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (  
+        <footer className="width-screen border-t border-gray-300 py-10 text-center">
+            <h6 className="text-xs lg:text-sm text-gray-300">©2025.krou.All rights reserved.</h6>
+        </footer>
+    );
+}
+ 
+export default Footer;
